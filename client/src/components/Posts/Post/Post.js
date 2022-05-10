7603d0395122d5d6ea3dde6260e2fad439d948fd
muratti32 <muratti32@gmail.com>
2022-05-10 15:02:54 +0300

    post card created

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,11 +1,18 @@
-import { Button, Card, CardMedia } from '@material-ui/core';
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Typography,
+} from '@material-ui/core';
 import { common, posts } from 'components/store/selectors';
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { ThumbUpAlt, Delete, MoreHoriz } from '@material-ui/icons';
 
-import { getPosts } from '../thunk';
 import useStyles from './styles';
+import moment from 'moment';
 
 const Post = (props) => {
   const postData = props.post;
@@ -13,12 +20,54 @@ const Post = (props) => {
   const dispatch = useDispatch();
   const loading = useSelector(common.loading);
   const post = useSelector(posts.postList);
-  console.log(`halo posts:`, postData);
+  console.log(`halo postData?.tags:`, postData);
   return (
     <Card className={classes.card}>
       <CardMedia
         className={classes.media}
-        image={postData.selectedFile}></CardMedia>
+        image={
+          postData.selectedFile ||
+          'https://user-images.githubusercontent.com/194400/49531010-48dad180-f8b1-11e8-8d89-1e61320e1d82.png'
+        }
+        title={postData.title}
+      />
+      <div className={classes.overlay}>
+        <Typography variant="h5"> {postData?.creator} </Typography>
+        <Typography variant="body2">
+          {moment(postData?.createdAt).fromNow()}
+        </Typography>
+      </div>
+      <div className={classes.overlay2}>
+        <Button style={{ color: 'white' }} size="small" onClick={() => {}}>
+          <MoreHoriz fontSize="medium" />
+        </Button>
+      </div>
+      <div className={classes.details}>
+        <Typography variant="body2" color="primary">
+          {postData?.tags.map((tag) => `#${tag} `)}
+        </Typography>
+      </div>
+      <Typography
+        className={classes.title}
+        gutterBottom
+        variant="h5"
+        component="h2">
+        {post.title}
+      </Typography>
+
+      <CardContent>
+        <Typography variant="h5" className={classes.title} gutterBottom>
+          {postData.message}
+        </Typography>
+      </CardContent>
+      <CardActions className={classes.cardActions}>
+        <Button size="small" color="primary" onClick={() => {}}>
+          <ThumbUpAlt fontSize="small" /> Like {postData.likeCount}
+        </Button>
+        <Button size="small" color="primary" onClick={() => {}}>
+          <Delete fontSize="small" />
+        </Button>
+      </CardActions>
     </Card>
   );
 };
--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import { store } from "components/store/store";
-import { Provider } from "react-redux";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { store } from 'components/store/store';
+import { Provider } from 'react-redux';
+import './index.css';
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById("root")
+  document.getElementById('root'),
 );
