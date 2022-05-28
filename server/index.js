34521d9f779396bb877314acdf3396f3397f52af
muratti32 <muratti32@gmail.com>
2022-05-28 13:53:45 +0300

    add mobile compability

--- server/index.js
+++ server/index.js
@@ -3,20 +3,21 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import postsRouter from './routes/posts.js';
+import dotenv from 'dotenv';
 
+dotenv.config();
 const app = express();
+
 app.use(cors());
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 
 app.use('/posts', postsRouter);
 
-const CONNECTION_URL =
-  'mongodb+srv://murat:8UR4Fr73TWXUi2h2@cluster0.igdb9.mongodb.net/memories_project?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5001;
 
 mongoose
-  .connect(CONNECTION_URL)
+  .connect(process.env.CONNECTION_URL)
   .then(() => {
     console.log('Connected to database');
     app.listen(PORT, () => {
